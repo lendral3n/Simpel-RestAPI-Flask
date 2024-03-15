b3f635c1fd99f7cda5c8ec110ccb6556da7e190c
L 3 N <l3noneone@gmail.com>
2024-03-15 21:16:18 +0700

    create CRUD

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # Simpel RestAPI Flask
+This project is a simple REST API built with Flask and deployed on Heroku.
+
+## Deployment
+
+The API is deployed on Heroku at the following URL:
+```
+https://simpel-restful-api-flask-ce699c48376a.herokuapp.com
+```
+![main](docs/image.png)
+
+## Features
+
+- CRUD operations for user management.
+
+## Technologies
+
+- Flask
+- Flask-SQLAlchemy
+- Flask-MySQLdb
+- Flask-Marshmallow
+- Heroku
+
+## Installation
+
+To run this project locally, follow these steps:
+
+1. Clone the repository.
+2. Install the required dependencies using `pip install -r requirements.txt`.
+3. Run the Flask server with `python app.py`.
+
+## Usage
+
+The API provides the following endpoints:
+
+- `POST /users` - Create a new user.
+- `GET /users` - Retrieve all users.
+- `GET /users/<id>` - Retrieve a user by ID.
+- `PUT /users/<id>` - Update a user by ID.
+- `DELETE /users/<id>` - Delete a user by ID.
+
+
--- app.py
+++ app.py
@@ -1,37 +1,105 @@
-from flask import Flask, request
-from flask_restful import Resource, Api
-from flask_cors import CORS
+# Importing necessary libraries
+from flask import Flask, jsonify, request, json
+from flask_sqlalchemy import SQLAlchemy
+from flask_mysqldb import MySQL
+from flask_marshmallow import Marshmallow
 
-# inisialisasi object flask
+# Creating an instance of the Flask class
 app = Flask(__name__)
 
-#  inisialisasi object flask_restfull
-api = Api(app)
+# Initializing SQLAlchemy, Marshmallow, and MySQL
+db = SQLAlchemy()
+ma = Marshmallow()
+mysql = MySQL(app)
 
-#  inisialisasi object flask_cors
-CORS(app)
+# Defining the User model with its fields
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(15), nullable=False)
+    email = db.Column(db.String(120), nullable=False)
+    password = db.Column(db.String(15), nullable=False)
 
-# inisialisasi variabel global kosong tipe dicrionary
-identitas = {} 
+    # Constructor to initialize the User object
+    def __init__(self, name, email, password):
+        self.name = name
+        self.email = email
+        self.password = password
 
-# buat class resource
-class MyResource(Resource):
-    # method get dan post
-    def get(self):
-        # response = {"messages":"Halo Guys"}
-        # return response
-        return identitas
+# Defining the User schema for serialization
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'name', 'email', 'password')
 
-    def post(self):
-        nama = request.json["nama"]
-        umur = request.json["umur"]
-        identitas["nama"] = nama
-        identitas["umur"] = umur
-        response = {"messages":"Berhasil menambahkan data"}
-        return response
+# Creating instances of the UserSchema
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+
+# Configuring the database URI
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/user'
+
+# Initializing the app with the database settings
+db.init_app(app)
+
+# Creating the database tables within the application context
+with app.app_context():
+    db.create_all()
     
-#  setup resource nya
-api.add_resource(MyResource, "/home", methods=["GET", "POST"])
+# CRUD operations for the User model
+
+# Create a new user
+@app.route('/users', methods=['POST'])
+def add_user():
+    _json = request.json
+    name = _json['name']
+    email = _json['email']
+    password = _json['password']
+    new_user = User(name=name, email=email
+                    , password=password)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({"message":"Success Create User"})
+
+# Get All User
+@app.route('/users', methods=['GET'])
+def get_user():
+    all_user = User.query.all()
+    result = users_schema.dump(all_user)
+    return jsonify({"message":"Success Get User"}, result)
+
+# Get User By Id
+@app.route('/users/<id>', methods=['GET'])
+def get_user_by_id(id):
+    user = User.query.get(id)
+    if user is None:
+        return jsonify ({"error": "the user doesn't exist"})
+    else: 
+        idresult = users_schema.dump(user, many=False)
+        return jsonify({"message":"Success Get User By Id"}, idresult) 
+        
+# Update User By Id
+@app.route('/users/<id>', methods=['PUT'])
+def update_user(id):
+    user = User.query.get(id)
+    if user is None:
+        return jsonify ({"error": "the user doesn't exist"})
+    _json = request.json
+    user.name = _json['name']
+    user.email = _json['email']
+    user.password = _json['password']
+    db.session.commit()
+    return jsonify({"message":"Success Update User"})
+
+# Delete User By Id
+@app.route('/users/<id>', methods=['DELETE'])
+def delete_user(id):
+    user = User.query.get(id)
+    if user is None:
+        return jsonify ({"error": "the user doesn't exist"})
+    else:
+        db.session.delete(user)
+        db.session.commit()
+        return jsonify({"message": "Success Delete User"})
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
